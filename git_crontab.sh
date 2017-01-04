c957c2d686cbd7d7a55778a83474df1e6eec0b42
aoswce <aoswce@126.com>
2017-01-04 11:57:26 +0800

    git assoc

--- git_crontab.sh
+++ git_crontab.sh
@@ -1,3 +1,13 @@
 #! /bin/sh
-echo `git pull` >> /usr/local/yele-server/git_crontab.log &
-echo `date` >> /usr/local/yele-server/git_crontab.log &
+(/usr/bin/kill -9 `ps -ef | grep -v grep | grep zapi | awk '{print $2}'`  )  && 
+(
+cd /usr/local/yele-server/ &&
+
+/usr/bin/git pull >> /usr/local/yele-server/git_crontab.log &&
+
+/usr/bin/rm -rf webroot/zapi.pid &&
+
+/usr/bin/php webroot/main.php start >> /usr/local/yele-server/git_crontab.log  &&
+
+echo `date` >> /usr/local/yele-server/git_crontab.log
+)
