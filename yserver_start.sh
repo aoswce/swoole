8ee10cc2fbead08b7b264feb74de1ec573dc4883
aoswce <aoswce@126.com>
2017-01-05 11:29:13 +0800

    日志调试

---
+++ yserver_start.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+(/usr/bin/kill -9 `ps -ef | grep -v grep | grep Yserver | awk '{print $2}'` & )  &&
+(
+cd /usr/local/yele-server/ &&
+
+/usr/bin/php server/server.php >> /usr/local/yele-server/git_crontab.log  &&
+sleep 1
+/usr/bin/php server/serverClient.php >> /usr/local/yele-server/git_crontab.log  &&
+
+echo `date` >> /usr/local/yele-server/git_crontab.log
+)
